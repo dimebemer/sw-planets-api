7d1ed5338fde5179ec9c3ebb478c9c18c2a6f134
Rafael Bemerguy <rafabemerguy@gmail.com>
2019-07-11 19:01:11 -0300

    início projeto

---
+++ src/main/java/com/starwars/swplanetsapi/SwPlanetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.starwars.swplanetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPlanetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwPlanetsApiApplication.class, args);
+	}
+
+}
